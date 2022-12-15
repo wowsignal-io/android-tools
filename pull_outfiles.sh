e46232b744a82a18972eea673d0030ab8ce3eb95
Adam Sindelar <adam@wowsignal.io>
2022-12-15 13:37:54 -0800

    Fix formatting of PULL messages

--- pull_outfiles.sh
+++ pull_outfiles.sh
@@ -17,4 +17,4 @@ export -f __maybe_open
 adb shell find $REMOTE_OUT -mtime -1d -type f 2>/dev/null \
     | xargs basename \
     | xargs -I{} bash -c \
-        '[[ -f "${TARGET}/${1}" ]] || ( echo -ne "\n\033[32mPULL \033[34m${1}\n\033[0m" && adb pull "${REMOTE_OUT}/${1}" "${TARGET}/${1}" >/dev/null && __maybe_open "${1}" )' _ {}
+        '[[ -f "${TARGET}/${1}" ]] || ( echo -ne "\n\r\033[42mPULLED\033[0m \033[34m${1}\n\033[0m" && adb pull "${REMOTE_OUT}/${1}" "${TARGET}/${1}" >/dev/null && __maybe_open "${1}" )' _ {}
